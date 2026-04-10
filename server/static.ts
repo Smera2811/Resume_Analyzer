d6b0c1c05772d794727f06e95d1bf456aab45df3
Smera2811 <Smerapatel.uni@gmail.com>
2026-04-10 14:25:31 +0000

    Update static path to dist folder

--- server/static.ts
+++ server/static.ts
@@ -10,7 +10,7 @@ const __dirname = dirname(__filename);
 
 export function serveStatic(app: Express) {
   // On Render, the build folder is usually one level up from the server folder
-  const distPath = resolve(__dirname, "..", "public");
+  const distPath = resolve(__dirname, "..", "dist");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
